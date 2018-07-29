a815a83f1112746097e31b35b85ed1e7762121f0
Luis Armando Chávez Soto <lquetzalmx@gmail.com>
2018-07-29 17:55:45 -0500

    Initial Commit

---
+++ MemeSounds/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MemeSounds
+//
+//  Created by Luis Chávez on 29/07/18.
+//  Copyright © 2018 Luis Chávez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MemeSounds/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MemeSounds
+//
+//  Created by Luis Chávez on 29/07/18.
+//  Copyright © 2018 Luis Chávez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
